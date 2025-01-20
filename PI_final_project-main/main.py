6b42c88d9face6fcbb33c321fd33313899e67a9a
Artur-Ibatulin <154439756+Artur-Ibatulin@users.noreply.github.com>
2025-01-21 01:08:29 +0500

    update

---
+++ PI_final_project-main/main.py
@@ -0,0 +1,58 @@
+# Импорт необходимых библиотек
+import io
+import streamlit as st
+from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+
+
+# Загрузка модели
+# st.cache_data
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+# Функция для обработка изображения
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+# Функция для загрузки изображения
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+# Функция для предсказания типа изображения
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+# Создаем образец модели
+model = load_model()
+
+
+# Намечаем с помощью стримлит основные элементы страницы
+st.title('улучшенная классификации изображений в Streamlit')
+img = load_image()
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('Результаты распознавания:')
+    print_predictions(preds)
+
+# Коммит 1
---
+++ PI_final_project-main/test_main.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+from PIL import Image
+from main import load_model, preprocess_image, print_predictions
+
+
+class TestImageClassificationApp(unittest.TestCase):
+    def setUp(self):
+        self.model = load_model()
+
+    def test_load_model(self):
+        self.assertIsNotNone(self.model, "Модель не загружена")
+
+    def test_preprocess_image(self):
+        img = Image.new('RGB', (224, 224))
+        preprocessed_img = preprocess_image(img)
+        self.assertIsInstance(preprocessed_img, np.ndarray, "Изображение не было правильно предварительно обработано")
+
+    # Создаем тестовые предсказания с формой
+    def test_print_predictions(self):
+        preds = np.random.rand(1, 1000)
+        print_predictions(preds)
+        # Здесь мы предполагаем, что
+        # функция print_predictions выводит результаты в stdout,
+        # поэтому мы не можем напрямую проверить вывод.
+        # Вместо этого мы можем проверить, что функция не вызывает исключений.
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
